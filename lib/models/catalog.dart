7f6fc3f82c8389d4a3e2d54d68eb2aaf69c95ea6
Nitesh8x <guptanitesh932@gmail.com>
2021-11-09 17:09:01 +0530

    Day 10 Completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,27 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item(
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.color,
+      required this.image});
+}
+
+final products = [
+  Item(
+      id: "Product001",
+      name: "IPhone 12 Pro",
+      desc: "Apple IPhone 12th Generation",
+      price: 999,
+      color: "#33505a",
+      image:
+          "https://m.media-amazon.com/images/G/31/img20/Wireless/Apple/iPhone12/AMZ_FamiyStripe_iPhone_12mini._CB419233187_.png")
+];
