2c998579601bfdb0044c6291db9fbb90bbcb03cd
Nitesh8x <guptanitesh932@gmail.com>
2021-11-12 19:40:24 +0530

    Day 18 Completed

---
+++ lib/widgets/catalog_header.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_catalog/widgets/themes.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class CatalogHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        "Catalg App".text.xl5.bold.color(MyTheme.darkBluishColor).make(),
+        "Tending Products".text.xl2.make(),
+      ],
+    );
+  }
+}
