90dd4b30cc62b19aa8153ca97b9754c970a1caa9
Nitesh8x <guptanitesh932@gmail.com>
2022-06-17 19:21:32 +0530

    done

--- lib/Pages/cart_page.dart
+++ lib/Pages/cart_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/widgets/themes.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class CartPage extends StatelessWidget {
@@ -9,7 +8,7 @@ class CartPage extends StatelessWidget {
       backgroundColor: context.canvasColor,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
-        title: "Cart".text.make(),
+        title: "Cart".text.color(context.theme.accentColor).make(),
       ),
     );
   }
